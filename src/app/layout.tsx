67c2813c9d7f978c97c524ce3c3ed174779b0590
sarvesh371 <sarveshsingh.03@gmail.com>
2025-03-12 11:16:10 +0530

    added provider from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,10 +10,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{ children: React.ReactNode }>) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
